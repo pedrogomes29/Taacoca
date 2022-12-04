3acc7c0ee30d0db1e005528d6ca00c8427a09b75
Pedro Gomes <pedromgomes29@gmail.com>
2022-12-04 14:24:26 +0000

    Added moves

--- game.pl
+++ game.pl
@@ -39,39 +39,51 @@ pos_in_board((R,Q)):-
     between(-4,UpperBoundQ,Q).
 
 
-legal_move(GameState,(OldR,OldQ),(R,Q),Piece,StonesChosenPos):-
+other_stones(Stone,[Stone|OtherStones],OtherStones).
+
+other_stones(Stone,[X|R],OtherStones):-
+    other_stones(Stone,R,OtherStonesRest),
+    OtherStones=[X|OtherStonesRest].
+
+
+legal_move_stone(GameState,(R,Q),Piece,Stones,Stone):-
+    other_stones(Stone,Stones,OtherChosenStones),
+    (OldR,OldQ)=Stone,
     NewR is OldR+R,
     NewQ is OldQ+Q,
     getPiece(GameState,(NewR,NewQ),PieceInNewPos),
     (PieceInNewPos\=Piece;
-    member((NewR,NewQ),StonesChosenPos)),
-    pos_in_board((NewR,NewQ)).
+    member((NewR,NewQ),OtherChosenStones)),
+    pos_in_board((NewR,NewQ)).  
+
+
+legal_move(GameState,Stones,Move,Piece):-
+    maplist(legal_move_stone(GameState,Move,Piece,Stones),Stones).
 
 
 
-get_moves(GameState,Pos,Piece,OldMoves,StonesChosenPos,NewMoves):-
-    findall(Move,(member(Move,OldMoves),legal_move(GameState,Pos,Move,Piece,StonesChosenPos)),NewMoves).
+get_moves(GameState,Piece,Stones,InitialMoves,PossibleMoves):-
+    findall(Move,(member(Move,InitialMoves),legal_move(GameState,Stones,Move,Piece)),PossibleMoves).
 
 
-choose_stone(_,_,4,Moves,Stones,Moves,Stones):-!.
+choose_stone(_,_,4,Stones,Stones):-!.
 
-choose_stone(GameState,Player,PieceNr,OldMoves,StonesChosenPos,NewMoves,Stones):-
+choose_stone(GameState,Player,PieceNr,StonesChosenSoFar,Stones):-
     repeat,
     format('Enter stone ~d\'s position\n',[PieceNr]),
     write('Line: '),
     read(Line),
     write('Col: '),
     read(Col),
-    posToAxial((Line,Col),PosAxial),
-    getPiece(GameState,PosAxial,Piece),
-    Piece=Player,
-    get_moves(GameState,PosAxial,Player,OldMoves,StonesChosenPos,CurrentPossibleMoves),
+    posToAxial((Line,Col),Stone),
+    getPiece(GameState,Stone,Player),
     NewPieceNr is PieceNr+1,
-    choose_stone(GameState,Player,NewPieceNr,CurrentPossibleMoves,[PosAxial|StonesChosenPos],NewMoves,Stones).
+    choose_stone(GameState,Player,NewPieceNr,[Stone|StonesChosenSoFar],Stones).
 
 
 
 choose_stones(GameState,Player,PossibleMoves,Stones):-
+    choose_stone(GameState,Player,1,[],Stones),
     E = (0,1),
     SE = (1,0),
     SW = (1,-1),
@@ -79,7 +91,8 @@ choose_stones(GameState,Player,PossibleMoves,Stones):-
     NW = (-1,0),
     NE = (-1,1),
     InitialMoves=[E,SE,SW,W,NW,NE],
-    choose_stone(GameState,Player,1,InitialMoves,[],PossibleMoves,Stones).
+    get_moves(GameState,Player,Stones,InitialMoves,PossibleMoves).
+
 
 
 display_move((0,1)):-write('East (E)'),nl.
@@ -154,30 +167,36 @@ setPiece([Line|Rest],R,Q,Piece,NewBoard):-
     NewBoard=[Line|NewRest].
 
 
-
-moveStone(Board,Piece,(OldR,OldQ),(R,Q),NewBoard):-
+moveStone(Board,Piece,(OldR,OldQ),(R,Q),MovedStones,NewBoard,(NewR,NewQ)):-
+    member((OldR,OldQ),MovedStones),!,
     NewR is OldR+R,
-    NewQ is OldQ+Q, 
+    NewQ is OldQ+Q,
+    setPiece(Board,NewR,NewQ,Piece,NewBoard).
+
+
+moveStone(Board,Piece,(OldR,OldQ),(R,Q),MovedStones,NewBoard,(NewR,NewQ)):-
+    NewR is OldR+R,
+    NewQ is OldQ+Q,
     setPiece(Board,OldR,OldQ,empty,AuxBoard),
     setPiece(AuxBoard,NewR,NewQ,Piece,NewBoard).
 
     
-move(GameState, Player, [], Move, GameState).
+move(GameState, Player, [], Move, _ , GameState).
 
-move(GameState, Player, [Stone|R], Move, NewGameState):-
-    moveStone(GameState,Player,Stone,Move,AuxGameState),
-    move(AuxGameState,Player,R,Move,NewGameState).
+move(GameState, Player, [Stone|R], Move, MovedStones,NewGameState):-
+    moveStone(GameState,Player,Stone,Move,MovedStones,AuxGameState,NewPosition),
+    move(AuxGameState,Player,R,Move,[NewPosition|MovedStones],NewGameState).
 
 
 next_player(white,black).
 next_player(black,white).
 
 
-game_over(GameState-black, Winner):-
-    white_reach_opponent_home(GameState),Winner=white,!.
+game_over(GameState-black, white):-
+    white_reach_opponent_home(GameState).
 
-game_over(GameState-white, Winner):-
-    black_reach_opponent_home(GameState),Winner=black,!.
+game_over(GameState-white, black):-
+    black_reach_opponent_home(GameState).
 
 game:-
     initial_state(GameState-Player),
@@ -192,7 +211,7 @@ game_cycle(GameState-Player):-
 
 game_cycle(GameState-Player):-
     choose_move(GameState, Player, Stones, Move),
-    move(GameState, Player, Stones, Move, NewGameState),
+    move(GameState, Player, Stones, Move, [],NewGameState),
     next_player(Player, NextPlayer),
     display_game(NewGameState), !,
     game_cycle(NewGameState-NextPlayer).
